61573d9006ea6b52fda08200d795cbe53b5524db
Stanislav <ih3xcode@protonmail.com>
2022-07-21 02:16:18 +0300

    add fallback handler

--- whiteproto/__init__.py
+++ whiteproto/__init__.py
@@ -3,6 +3,7 @@
 from whiteproto.client import open_connection
 from whiteproto.server import WhiteServer
 from whiteproto.connection import WhiteConnection
+from whiteproto._proto import CloseConnectionReason
 
 
-__all__ = ["open_connection", "WhiteServer", "WhiteConnection"]
+__all__ = ["open_connection", "WhiteServer", "WhiteConnection", "CloseConnectionReason"]
--- whiteproto/server.py
+++ whiteproto/server.py
@@ -5,16 +5,22 @@ import asyncio
 from typing import Awaitable, Callable
 
 from whiteproto.connection import WhiteConnection, PeerType
-from whiteproto._proto import BUFFER_SIZE
+from whiteproto._proto import BUFFER_SIZE, CloseConnectionReason
 
 logger = logging.getLogger(__name__)
+
+
+async def _fallback_handler(connection: WhiteConnection):
+    # close all connections if no callback set
+    logger.error("No callback set, closing connection")
+    await connection.close(CloseConnectionReason.OK)
 
 
 class WhiteServer:
     """WhiteProto server"""
 
-    server: asyncio.AbstractServer
-    on_connection: Callable[[WhiteConnection], Awaitable[None]]
+    _server: asyncio.AbstractServer
+    _on_connection: Callable[[WhiteConnection], Awaitable[None]]
 
     def __init__(
         self: "WhiteServer", host: str, port: int, preshared_key: bytes
@@ -23,16 +29,17 @@ class WhiteServer:
         self.port = port
         self.loop = asyncio.get_event_loop()
         self.preshared_key = preshared_key
+        self._on_connection = _fallback_handler
 
     def set_callback(
         self: "WhiteServer", callback: Callable[[WhiteConnection], Awaitable[None]]
     ) -> None:
         """Sets callback for new connections"""
-        self.on_connection = callback
+        self._on_connection = callback
 
     async def start(self: "WhiteServer") -> None:
         """Starts server"""
-        self.server = await asyncio.start_server(
+        self._server = await asyncio.start_server(
             self._handle_client, self.host, self.port, limit=BUFFER_SIZE
         )
         logger.info("Server started on %s:%d", self.host, self.port)
@@ -45,10 +52,10 @@ class WhiteServer:
             logger.error("Failed to initialize connection")
             return
         logger.info("Initialized!")
-        await self.on_connection(connection)
+        await self._on_connection(connection)
 
     async def serve_forever(self: "WhiteServer") -> None:
         """Serves connections forever"""
-        if not self.server:
+        if not self._server:
             raise RuntimeError("Server not started")
-        await self.server.serve_forever()
+        await self._server.serve_forever()
